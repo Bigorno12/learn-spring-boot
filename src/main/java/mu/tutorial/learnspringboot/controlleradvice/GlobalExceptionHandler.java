59a9d0985a362283d3838b5594a0c28deee7b9d5
Pranav <pseew007@icloud.com>
2023-09-27 23:18:42 +0400

    Add RestControllerAdvice

--- src/main/java/mu/tutorial/learnspringboot/controlleradvice/GlobalExceptionHandler.java
+++ src/main/java/mu/tutorial/learnspringboot/controlleradvice/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package mu.tutorial.learnspringboot.controlleradvice;
 
 import mu.tutorial.learnspringboot.exception.ErrorMessage;
+import mu.tutorial.learnspringboot.exception.JsonPlaceHolderException;
 import mu.tutorial.learnspringboot.exception.UserException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,6 +18,17 @@ public class GlobalExceptionHandler {
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ErrorMessage userNotFoundHandler(UserException ex, WebRequest webRequest) {
         return ErrorMessage.builder()
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .timestamp(LocalDateTime.now())
+                .message(ex.getMessage())
+                .description(webRequest.getDescription(false))
+                .build();
+    }
+
+    @ExceptionHandler(JsonPlaceHolderException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorMessage jsonPlaceHolderBadRequestHandler(JsonPlaceHolderException ex, WebRequest webRequest) {
+            return ErrorMessage.builder()
                 .statusCode(HttpStatus.NOT_FOUND.value())
                 .timestamp(LocalDateTime.now())
                 .message(ex.getMessage())
